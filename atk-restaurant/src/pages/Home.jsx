ec0a83389f258bb79cfb0bba90a9055a7256793d
Adam Khodier <khodieradam1297@gmail.com>
2023-03-15 20:54:47 -0400

    organized pages and components

--- atk-restaurant/src/pages/Home.jsx
+++ atk-restaurant/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Veggie from '../components/Veggie'
+import Popular from '../components/Popular'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Veggie/>
+      <Popular/>
+    </div>
   )
 }
 
