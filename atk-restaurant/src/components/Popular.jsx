54660bfb9bd8f834c7065dfd5f9300d58d906e90
Adam Khodier <khodieradam1297@gmail.com>
2023-03-22 18:24:28 -0400

    changed api

--- atk-restaurant/src/components/Popular.jsx
+++ atk-restaurant/src/components/Popular.jsx
@@ -33,10 +33,11 @@ function Popular() {
           }}>
           {popular.map((recipe) => {
             return(
-              <SplideSlide>
+              <SplideSlide key={recipe.id}>
               <Card>
                 <p>{recipe.title}</p>
                 <img src={recipe.image} alt="recipe"/>
+                <Gradient/>
               </Card>
               </SplideSlide>
             );
@@ -74,7 +75,23 @@ const Card = styled.div`
     transform: translate(-50%, 0%);
     color: white;
     width: 100%;
+    text-align: center;
+    font-weight: 600;
+    font-size: 1rem;
+    height: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 `;
+
+const Gradient = styled.div`
+  z-index: 3;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.5))
+
+`
 
 export default Popular
